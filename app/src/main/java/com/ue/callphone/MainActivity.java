bc6a179a4d2f8db158675eb0c2322ee9488cd4c0
mashang123456 <865909475@qq.com>
2017-06-01 23:04:29 +0800

    【】longhui20170601V1.1三种按键方法

--- app/src/main/java/com/ue/callphone/MainActivity.java
+++ app/src/main/java/com/ue/callphone/MainActivity.java
@@ -5,19 +5,22 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉窗口的title
+        //去掉标题栏注意这句一定要写在setContentView()方法的前面，不然会报错的
         setContentView(R.layout.activity_main);
 
         final EditText etNum = (EditText) findViewById(R.id.et_number);
+        //第一种按键：匿名内部类
         Button btCallPhone = (Button) findViewById(R.id.bt_callphone);
-
         btCallPhone.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
@@ -27,13 +30,46 @@ public class MainActivity extends Activity {
                  itCall.setAction(Intent.ACTION_CALL);
                  itCall.setData(Uri.parse("tel:" + number));
                  startActivity(itCall);
-/*
-                 Intent intent = new Intent();
-				 intent.setAction(Intent.ACTION_VIEW);
-				 intent.setData(Uri.parse("http://www.baidu.com"));
-				 startActivity(intent);
-*/
+                 //第二种按键:将功能单独封装成一个函数
+                 //callWeb();
              }
         });
+
+        //第二种按键:将功能单独封装成一个函数
+        Button bt2 = (Button) findViewById(R.id.bt2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callWeb();
+            }
+        });
+
+        //第三种按键:内部类
+        Button bt3 = (Button) findViewById(R.id.bt3);
+        bt3.setOnClickListener(new MyOnClickListener());
+
+
     }
+
+    //第二种按键:将功能单独封装成一个函数
+    private void callWeb() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://www.baidu.com"));
+        startActivity(intent);
+    }
+
+    //第三种按键:内部类
+    class MyOnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            Toast.makeText(MainActivity.this, "Toast提示", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
+
 }
